1677c83f1da8ab98fcfbc4f46a3b56e0c725daa5
Greg Morgan <gregm9999@gmail.com>
2018-12-18 03:03:05 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,24 +16,24 @@ class FiguresController < ApplicationController
     @title_ids=params[:figure][:title_ids]
     @landmark=params[:landmark]
     @landmark_ids=params[:figure][:landmark_ids]
-    @figure=Figure.create(name: params[:figure][:name])
+    @figure=Figure.create(:name => params[:figure][:name])
     if !@title[:name].empty?
-      title=Title.create(name=>@title[:name])
+      title=Title.create(:name=>@title[:name])
       @figure.titles << title
     end
     if @title_ids
       @title_ids.each do |title_id|
-        title=Title.find(title)
+        title=Title.find(title_id)
         @figure.titles << title
       end
     end
     if !@landmark[:name].empty?
-      landmark=Landmark.create(name=>@landmark[:name])
+      landmark=Landmark.create(:name=>@landmark[:name])
       @figure.landmarks << landmark
     end
     if @landmark_ids
       @landmark_ids.each do |landmark_id|
-        landmark=Title.find(landmark_id)
+        landmark=Landmark.find(landmark_id)
         @figure.landmarks << landmark
       end
     end
@@ -44,13 +44,21 @@ class FiguresController < ApplicationController
   get '/figures/:id/edit' do
     @figure=Figure.find(params[:id])
     @titles=Title.all
-    @landmark=Landmark.all
+    @landmarks=Landmark.all
     erb :'/figures/edit'
   end
 
   get '/figures/:id' do
     @figure=Figure.find(params[:id])
     erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure=Figure.find(params[:id])
+    @figure.update(params[:figure])
+    @figure.save
+    binding.pry
+    redirect to "/figures/#{@figure.id}"
   end
 
 end
