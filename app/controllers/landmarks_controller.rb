f7f4451cde69b54946755d2dad7b4d19f0925bc5
Greg Morgan <gregm9999@gmail.com>
2018-12-18 04:26:52 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -29,7 +29,8 @@ class LandmarksController < ApplicationController
   get '/landmarks/:id' do
     @landmark=Landmark.find(params[:id])
     erb :'landmarks/show'
-
+  end
+  
   get '/landmarks/:id' do
     @landmark=Landmark.find(params[:id])
     erb :'landmarks/show'
